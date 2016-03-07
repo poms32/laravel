a316f535a72e0a78c1578363f85c757cffe0a2fe
Romain Pommerolle <Romain@minideviaevent.home>
2016-03-07 18:10:38 +0100

    Deploy modifications 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5.via</div>
+                <div class="title">Laravel 5.IO <br> VIA</div>
             </div>
         </div>
     </body>
